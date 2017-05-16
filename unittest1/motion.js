bbc4eb2e3ea3914b7fbbc70ceb07bd64c9836955
djzam3 <djzam3@student.monash.edu>
2017-05-16 16:16:49 +1000

    Client side

--- unittest1/motion.js
+++ unittest1/motion.js
@@ -1,26 +1,17 @@
 //Constructor
 function Motion(time){
-	this._ms = time;
+	this._timeStamp = time;
 }
 
 //Calculate long motion
-Motion.prototype.getMotion = function(){
+Motion.prototype.getMotionType = function(){
 	const LONG_MOTION = 8000;
-	if (this._ms > 0){
-		var timeStamp; //motion start and end time
-		timeStamp = new Date().getTime();
-
-		var start = new Date().getTime();
-		var end = start;
-		while(end < start + this._ms) { //Wait for time specified
-			end = new Date().getTime();
-		}
-		timeStamp = new Date().getTime() - timeStamp;
-		/*false indicates short motion*/
-		var longMotion = ( timeStamp >= LONG_MOTION ); 
-		return longMotion;
-	}
-	return -1; //no motion
+	if (this._timeStamp > LONG_MOTION) {
+            return "L";
+        }
+        else {
+            return "S";
+        }
 };
 
 module.exports = Motion;
