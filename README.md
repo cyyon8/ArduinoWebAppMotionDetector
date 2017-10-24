9de9d4df9980cb3a0c9cfd9f07499a3cc01bc2f8
James <jamsawamsa@gmail.com>
2017-10-24 14:16:51 +0800

    reorganize folder structure

---
+++ README.md
@@ -0,0 +1,134 @@
+# assignment-5-team-62
+# Morse code interpreter
+
+The morse code interpreter uses an Arduino Microcontroller and a pip motion sensor to capture motions and interpret chains of motions into letters, numbers and puncuation mark based on the morse code defined alphabet. It features:
+
+  - Ability to interpret motions into meaningful messages
+  - View printed messages through a web interface
+  - Ability to end transmissions, turn sensor and LED on or off through an easy to use web interface  
+  - Follows the standard morse code alphabet
+  - Real time server-client interaction
+
+### Dependencies & Tech
+
+The Arduino Web Controller uses a number of different packages and libraries in order to function properly:
+
+ - [socket.io](https://socket.io/) - enables real-time bidirectional event based communication between the server and the microcontroller, the version used is 1.7.3.
+ - [Express](https://expressjs.com/) - fast node.js network app framework, the version used 4.15.2.
+ - [johnny-five](http://johnny-five.io/) - Javascript Robotics and IoT platform utilizing node.js, version 0.10.6
+ - [Node.js](https://nodejs.org/en/) - lightweight and efficient Javascript runtime, version 7.5.0
+ 
+For testing:
+ - [mocha](https://mochajs.org/) a feature-rich JavaScript test framework , version 3.4.1
+ - [chai](http://chaijs.com/) - a BDD / TDD assertion library paired with any javascript testing framework, version 4.0.0
+
+An Arduino microcontroller connected to your computer is also required. Be sure to have uploaded the StandardFirmataPlus program into the board before you run the web server.
+
+Refer to the **Getting Started** section below for a step-by-step walk through on how to get your web server set up and running.
+
+## Getting Started
+
+### Installation
+
+The Arduino Web Controller requires [Node.js](https://nodejs.org/en/), [johnny-five](http://johnny-five.io/), [Express](https://expressjs.com/) and [socket.io](https://socket.io/) to run. It is recommended that you do include all of the packages excluding Node.js in one single directory. For simplicity's sake we assume that the user is using a Ubuntu bassed Linux distribution.
+
+Install Node.js
+
+```sh
+$ sudo apt-get install -y nodejs
+```
+Or you can follow the instructions [here](https://nodejs.org/en/download/package-manager/) to install it.
+
+Install the other dependencies. 
+
+```sh
+$ npm install express
+$ npm install socket.io
+$ npm install johnny-five
+$ npm install mocha -g
+$ npm install chai --save-dev
+```
+Note: mocha and chai are required to run the test cases only.
+
+### Setting Up The Arduino Uno
+The minimum requirements for the Arduino is 
+1. 1 PIR motion sensor
+2. 1 Arduino Uno
+3. 1 LED light
+4. 3 jumper cables
+
+For the PIR motion sensor, connect the VCC pin the 5V pin port, the GND pin to a GND port and the OUT pin to pin number 7 on the Arduino Uno. 
+[Here](http://johnny-five.io/examples/motion/)'s an example of setting up the motion sensor with the board.
+
+Connect the LED to a GND pin and pin number 13. Once the board has been set up, upload the StandardFirmataPlus.ino program into the board with the Arduino IDE.
+[Here](http://johnny-five.io/examples/led/)'s an example of how to set up the LED.
+
+Your board is now ready to go!
+### Getting the server running
+Once you have all the dependencies installed and the board set up, run the main.js file.
+```sh
+$ node app.js
+```
+If you are running this on a local machine, connect to http://localhost:3000 and you should see the control panel for the microcontroller.
+
+### Running unit tests
+Again, once all dependencies are installed including mocha and chai you will need to ensure you are currently in the unittest directory. 
+To run the unit tests use, navigate to the source code folder and use:
+```
+$ npm test
+```
+
+Alternatively, you can run each unit test separately. To run the first unit test:
+```sh
+$ mocha unitTest1.js
+```
+unitTest1 tests the functionality of the getMotionType() function, which basically decides if a detected motion is a short or long motion.
+And to run the second unit test:
+```sh
+$ mocha unitTest2.js
+```
+unitTest2 tests the functionality of the Morse Code decoder, which converts short and long motion signals into a characters.
+The board does not to be connected to perform these unit tests.
+
+## Features and functions
+
+### Morse Code Interpreter
+The main functionality is the ability to decode motions into meaningful messages through short and long motions. A long motion is defined as any motion greater than or equal to 8 seconds and a short motion is below 8 seconds. 
+
+```js
+'A': 'SL'
+'B':'LSSS'
+'C':'LSLS'
+...
+'Z':'LLSS'
+'SK': 'SSSLSL' //end of transmission
+```
+
+From each transmission of the motion to the client side, the system will check when the last time signal is sent through with the motion, if the gap is 3 seconds long than the word is still being produced, and a 7 second gap is to indicate that a word has been completed. From this the system will print out when ever a letter has been decoded and/or space between words. 
+
+### Web interface
+
+The web interface allows you turn off transmission, which will cause the system to stop getting information from the server. It also allows you turn off the LED and Motion sensor.
+
+Turn the LED switch on to switch the LED on. 
+The LED has 2 modes:
+ - The first mode is *only* when the LED is switched on. In this case the LED switch behaves as a regular on/off switch and will turn the LED on or off regardless of any motions detected.
+ - The second mode is when *both* the motion sensor and the LED are switched on. This time the LED will only flash when a motion is detected.
+
+Turn the motion sensor switch on to enable motion detection. When a motion is detected, the counters will increase. A short motion is defined as a motion lasting under 8 seconds, and motions above 8 seconds are considered long motions. When the LED is switched on, the the LED will flash whenever a motion is detected. Motions detected when the LED is switched off will still update the counters but the LED will not flash. 
+
+When the motion sensor is turned off whilst detecting a motion, it will end the current motion and the controller will calculate the motion's duration based on the time it started to when the motion sensor is switched off.
+
+Switching the motion sensor on is equivalent to starting the signal recording session. When you turn the motion sensor switch off, the program will take whatever signals that are currently recorded from the current session and will proceed to decode the message. After that the message will be displayed on the user interface and the array that stores the signals will be emptied.
+
+### Known Bugs
+
+Because the motion detector will constantly detect motions when connected to the Arduino, there will be times when a motion is already detected even before turning the motion sensor switch on. Turning the motion sensor on in the middle of this will cause the controller to register the start time of the motion as when the switch was turned on, not when the actual motion began. 
+
+Turning the motion sensor off in the middle of a detection will cause the controller to tabulate its duration based on when the motion started until the switch was turned off. Turning the switch on again immediately after might cause the program to register the single long motion as 2 motions.
+
+The motion detector might also not be able to accurately detect motion from the sensor. We have tried stimulating the motion sensor for about 10 seconds uninterrupted, but there are still times when the motion sensor would not detect anything, or end it's current detection session.
+
+### Authors
+ - James Lee Zhong Kein - https://github.com/jamsawamsa
+ - Daniel Zambetto - https://github.com/djzam3
---
+++ app.js
@@ -0,0 +1,283 @@
+'use strict';
+
+// Requirements and dependencies declarations
+const five = require('johnny-five');
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+// Initializing LED and motion sensor variables
+let led = null;
+let motion = null;
+
+
+
+// File resource pointers
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res, next) {
+  res.sendFile(__dirname + '/index.html')
+});
+
+//Boolean for motion sensor switch
+var motionSensorSwitch = false, ledSwitch = false;
+var dot = 2; // adjusts the time sensitivity between words and chars 
+var signalArray = []; // array to store signals
+var WORDGAP = 7000 * dot; // global vars for word and char gaps
+var CHARGAP = 3000 * dot;
+var skRecv = 0; // whether the SK prosign has been received
+var timeGap = 0; // time gap between current and previous signal event
+var timeGapOffset = 0; // offset for the time gap
+
+
+var morseCharacterToEncodingTable = { // morse code table
+        'A': 'SL',
+        'B': 'LSSS',
+        'C': 'LSLS',
+        'D': 'LSS',
+        'E': 'S',
+        'F': 'SSLS',
+        'G': 'LLS',
+        'H': 'SSSS',
+        'I': 'SS',
+        'J': 'SLLL',
+        'K': 'LSL',
+        'L': 'SLSS',
+        'M': 'LL',
+        'N': 'LS',
+        'O': 'LLL',
+        'P': 'SLLS',
+        'Q': 'LLSL',
+        'R': 'SLS',
+        'S': 'SSS',
+        'T': 'L',
+        'U': 'SSL',
+        'V': 'SSSL',
+        'W': 'SLL',
+        'X': 'LSSL',
+        'Y': 'LSLL',
+        'Z': 'LLSS',
+        'SK': 'SSSLSL',
+        '1': 'SLLLL',
+        '2': 'SSLLL',
+        '3': 'SSSLL',
+        '4': 'SSSSL',
+        '5': 'SSSSS',
+        '6': 'LSSSS',
+        '7': 'LLSSS',
+        '8': 'LLLSS',
+        '9': 'LLLLS',
+        '0': 'LLLLL',
+        '.': 'SLSLSL',
+        ',': 'LLSSLL',
+        '?': 'SSLLSS',
+        "'": 'LSSSSL',
+        '!': 'LSLSLL',
+        '/': 'LSSLS',
+        '(': 'LSLLS',
+        ')': 'LSLLSL',
+        '&': 'SLSSS',
+        ':': 'LLLSSS',
+        ';': 'LSLSLS',
+        '=': 'LSSSL',
+        '+': 'SLSLS',
+        '-': 'LSSSSL',
+        '_': 'SSLLSL',
+        '"': 'SLSSLS',
+        '$': 'SSSLSSL',
+        '@': 'SLLSLS'
+};
+
+
+function beginMotion() {
+    if (signalArray.length == 0) { // adjust offset for the first signal
+        timeGapOffset = new Date().getTime();
+        timeGap = 0; 
+    } else { // get new time gap
+        timeGap = new Date().getTime() - timeGap - timeGapOffset; // get new time gap
+    }
+    if (motionSensorSwitch) {
+            // console.log("Motion detected");
+            io.emit("motion:start"); // initiate a motion starting in client side
+    }
+    if (ledSwitch) {
+        led.on();
+    }
+}
+
+//User story 6b
+function getCharacter (signal) { // gets a character from the morse code table from a string of signals
+    if (signal.length == 0) { // check for a signal
+        return null;
+    }
+    else {
+        var keys = Object.keys( morseCharacterToEncodingTable ); // get dictionary key indices
+        for (var i = 0; i <Object.keys(morseCharacterToEncodingTable).length; i++) { // check for a match
+            if (signal == morseCharacterToEncodingTable[keys[i]]) {
+                return keys[i];
+            }
+        }
+        return null;
+    }
+}
+
+function decode (events) { // decodes signals into a message
+    var build = [], message = [], word =[], char; //define vars
+    for (var i = 0; i < events.length; i++) { // loop while there are still signals to process
+        if (events[i].gap < 0) { // handle invalid time gaps
+            return "Error: Signal interval time is not positive.";
+        }
+        if (events[i].gap >= WORDGAP) { // words over here
+            char = getCharacter(build.join("")); // get character
+            if (char == null) word.push("null"); // null characters
+            else {word.push(char);} // push char to word array
+            message.push(word.join("")); // push word array to msg array
+            build = [], word = []; // clear build and word arrays
+            if (message.length != 0) message.push(" "); // if not the first word, add a space to the message
+        }
+        else if (events[i].gap >= CHARGAP) { // letters here
+            char = getCharacter(build.join("")); // get character
+            if (char == "SK") { // if receive end of message, exit early
+                message.push(word.join(""));
+                message.push('SK')
+                //User story 6a
+                return message; 
+            }
+            if (build.length != 0) word.push(char); // if build array is not empty
+            build = [];
+        }
+        build.push(events[i].signal); // every iteration adds the current signal to the build array
+        if (i == events.length -1) { // if at the end of the signals
+            if (build.length != 0) { // computes remaining chars into the last letter
+                char = getCharacter(build.join(""))
+                if (char == 'SK') { // SK read
+                    message.push(word.join("")); //early exit
+                    message.push('SK');
+                    return message;
+                }
+                else if (char != null)  word.push(char);
+                else word.push("null");
+            }
+            if ( word.length != 0) { // if there are remainining words in the word array
+                message.push(word.join("")); // push the word to message array
+            }
+        }
+    }
+    return message; // returns the message
+}
+
+//Arduino board connection
+var board = new five.Board();  
+board.on("ready", function() {  
+    console.log('Arduino connected');
+    // Declaring pins used on the board
+    led = new five.Led(13);
+    motion = new five.Motion(7);
+    led.off();
+
+    //User story 1a
+    motion.on("calibrated", function() {
+        console.log("Motion sensor calibrated")
+    });
+    //User story 1b
+    motion.on("motionstart", function() {
+        beginMotion(); // initiate the beginning of a motion detection
+    });
+
+    motion.on("motionend", function() {
+        if (motionSensorSwitch) { // if motion switch is on
+            io.emit("motion:end"); // initiate a motion ending
+        }
+        if (!motionSensorSwitch && ledSwitch) { // keep led on if led switch is on
+            led.on();
+        }
+        else if (ledSwitch) { // turn led off otherwise
+            led.off();
+        }
+    });
+});
+
+io.on('connection', function (client) {
+    client.on('join', function(handshake) {
+        console.log("Client connected");
+        motionSensorSwitch = false;
+        ledSwitch = false;
+    });
+    client.on('led:on', function (data) { // led switched on from client side
+        ledSwitch = true; 
+        if (motionSensorSwitch && motion.detectedMotion){ // if currently detecting a motion
+            led.on(); // switch led on
+        }
+        else if (!motionSensorSwitch) { // switch on if not detecting a motion
+            led.on();
+        }        
+        console.log('LED ON RECEIVED');
+    });
+ 
+    client.on('led:off', function (data) { // led switched off from client side
+        ledSwitch = false;
+        led.off(); // switch off led
+        console.log('LED OFF RECEIVED');
+    });
+
+    client.on('motion:on', function(data) { // motion switched on from client side
+        console.log("MOTION SENSOR ON RECEIVED");
+        motionSensorSwitch = true;
+        timeGapOffset = new Date().getTime() - timeGap; // resuming motion detection, update the time gap to ignore break period
+        if (motion.detectedMotion) {
+            beginMotion(); // initiate a motion detection sequence if a motion is detected
+        }
+    });
+
+    // motion switched off from client side
+    client.on('motion:off', function(data) {
+        console.log(signalArray);
+        console.log("MOTION SENSOR OFF RECEIVED");
+        // save time gap at this instance
+        timeGap = new Date().getTime() - timeGap - timeGapOffset;
+        if (ledSwitch) {
+            led.on();
+        }
+        // initiate motion end sequence in case any motion is currently being detected
+        io.emit('motion:end');
+        motionSensorSwitch = false;
+    });
+
+    // an event needs to be updated
+    client.on('eventUpdate',function(data) {
+        // signalArray.push({signal:data.motionType, gap:parseInt(data.timeStamp)});
+        signalArray.push({signal:data.motionType, gap: timeGap});
+        timeGap = new Date().getTime() - timeGapOffset;
+        console.log(signalArray);
+        if (signalArray.length != 0 && skRecv == 0) { // if there are signals and prosign not received
+            var decodedMsg = decode(signalArray); // decode signals
+            console.log(decodedMsg);
+            if (decodedMsg[decodedMsg.length-1] == 'SK') {
+                skRecv = 1;
+                decodedMsg = decodedMsg.splice(decodedMsg.length-1, 1); // remove last element in array
+                decodedMsg = "SK received, no further signals will be decoded <br>The decoded message is:" + decodedMsg.join(""); // set msg to be printed
+            }
+            else {
+                // set the msg to be shown
+                decodedMsg = "The decoded message is: <br>" + decodedMsg.join("");
+            }
+            // tell client to display message
+            io.emit('messageDecoded', {decodedMsg}); // let client side know that there is a decoded message
+        }
+    });
+
+    client.on('resetDb', function(data) {
+        console.log('Database reset.')
+        signalArray = [];
+        skRecv = 0; // reset sk received and signal array
+    });
+}); 
+
+
+
+// Declare port
+//User story 5a
+const port = process.env.PORT || 3000;
+
+server.listen(port);
+console.log(`Server listening on http://localhost:${port}`);
